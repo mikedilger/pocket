9a2f7a5f5352018cb4db1c7c9da229f7a2720274
Mike Dilger <mike@mikedilger.com>
2024-04-13 09:29:02 +1200

    Error types (with source location)

---
+++ pocket-types/src/error.rs
@@ -0,0 +1,59 @@
+use std::error::Error as StdError;
+use std::panic::Location;
+
+#[derive(Debug)]
+pub struct Error {
+    pub inner: InnerError,
+    location: &'static Location<'static>,
+}
+
+impl StdError for Error {
+    fn source(&self) -> Option<&(dyn StdError + 'static)> {
+        Some(&self.inner)
+    }
+}
+
+impl std::fmt::Display for Error {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}, {}", self.inner, self.location)
+    }
+}
+
+/// Errors that can occur in the crate
+#[derive(Debug)]
+pub enum InnerError {
+    General(String),
+}
+
+impl std::fmt::Display for InnerError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            InnerError::General(s) => write!(f, "{s}"),
+        }
+    }
+}
+
+impl StdError for InnerError {
+    fn source(&self) -> Option<&(dyn StdError + 'static)> {
+        match self {
+            _ => None,
+        }
+    }
+}
+
+// Note: we impl Into because our typical pattern is InnerError::Variant.into()
+//       when we tried implementing From, the location was deep in rust code's
+//       blanket into implementation, which wasn't the line number we wanted.
+//
+//       As for converting other error types, the try! macro uses From so it
+//       is correct.
+#[allow(clippy::from_over_into)]
+impl Into<Error> for InnerError {
+    #[track_caller]
+    fn into(self) -> Error {
+        Error {
+            inner: self,
+            location: std::panic::Location::caller(),
+        }
+    }
+}
